4d03fef105ad357b2ac516cd1b83e1c6a5432bd1
Guilherme Campos <guiicampos31@gmail.com>
2024-12-10 16:45:56 -0300

    Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-
+        Department obs = new Department(1, "teste");
+        System.out.println(obs);
     }
 }
