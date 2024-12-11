a8f7a8ee8d85ad302eb073d4cb43c93f631305dc
Guilherme Campos <guiicampos31@gmail.com>
2024-12-11 14:48:13 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Department obs = new Department(1, "teste");
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obs);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
