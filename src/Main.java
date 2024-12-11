830908efa25b5edf3258d87b422112da773792cc
Guilherme Campos <guiicampos31@gmail.com>
2024-12-11 12:20:15 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obs = new Department(1, "teste");
-        System.out.println(obs);
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obs);
+        System.out.println(seller);
     }
 }
